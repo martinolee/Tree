3904c83999d2594115c7ae9e04578dd98800d3e4
martinolee <soohan.m.lee@gmail.com>
2024-12-05 15:51:16 +0900

    Add Xcode project

---
+++ Tree/Tree/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tree
+//
+//  Created by Soohan Lee on 12/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundStyle(.tint)
+      Text("Hello, world!")
+    }
+    .padding()
+  }
+}
+
+#Preview {
+  ContentView()
+}
---
+++ Tree/Tree/TreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TreeApp.swift
+//  Tree
+//
+//  Created by Soohan Lee on 12/5/24.
+//
+
+import SwiftUI
+
+@main
+struct TreeApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
